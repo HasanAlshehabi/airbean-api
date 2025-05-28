0504bbbb20d29c7303b0773feb066f784a452821
HasanAlshehabi <Hasan.alshehabi99@gmail.com>
2025-05-28 12:38:22 +0200

    added start code to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection;
+db.on("error", (error) => {console.error("Connection error:", error);});
+db.once("open", () => {console.log("Connected to MongoDB");
+});
+   app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+   });
