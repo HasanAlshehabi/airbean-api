f37af18a0875ceb85b24f6dc60abb72008299966
Axner <@axnerhagglund@gmail.com>
2025-05-28 15:56:12 +0200

    routing för auth

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+import Router from "express"
+
+const router = Router()
+
+
+
+
+
+//POST Login
+router.post("/login", (req,res) => {})
+//GET Logout
+router.get("/logout", (req,res) => {})
+//POST Register
+router.post("/register", (req,res) => {})
+
+
+
+
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from "./routes/auth.js"
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+
+
 app.use(express.json());
+
+
+//routes 
+app.use("/api/auth", authRouter )
 
 
 mongoose.connect(process.env.MONGODB_URI);
