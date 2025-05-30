ba511b9628f24f991ab092aae337a478a8566f35
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-05-30 10:45:33 +0200

    Added getter for userID

--- globalActiveUser/globalActiveUser.js
+++ globalActiveUser/globalActiveUser.js
@@ -4,6 +4,10 @@ export const getActiveUser = () => {
   return global._activeUser;
 };
 
+export const getActiveUserId = () => {
+  return global._activeUser.userId;
+};
+
 export const setActiveUser = (user = null) => {
   if (user && typeof user === "object") {
     global._activeUser = user;
