ad75c623f818fba5b53f048c20bb16ce0d3b3cf4
Nabeel Mansour <mikaeel1@hotmail.com>
2025-06-02 13:09:20 +0200

    added route to server.js

--- routes/cart.js
+++ routes/cart.js
@@ -1,16 +1,38 @@
 import express from "express";
-import { Cart } from "../models/Cart.js";
+import Cart from "../models/Cart.js";
 
 const router = express.Router();
 
 // GET all carts
 router.get("/", async (req, res) => {
-  const cart = await Cart.find();
-  res.json(cart);
+  try {
+    const cart = await Cart.find();
+    res.json(cart);
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "No cart could be found",
+    });
+  }
 });
 
 // hämta cart med given cartId
 router.get("/:cartId", async (req, res) => {
-  const cart = await Cart.find({ userId: req.params.userId });
-  res.json(cart);
+  try {
+    const cart = await Cart.find({ userId: req.params.userId });
+    if (!cart) {
+      res.status(400).json({
+        success: false,
+        message: "No cart was found with provided ID",
+      });
+    }
+    res.json(cart);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Could not get the Cart",
+    });
+  }
 });
+
+export default router;
